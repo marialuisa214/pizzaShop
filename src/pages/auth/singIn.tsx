80d87b16c87da0fb81b3d09aa8b6d0d201843426
Maria Luisa <maria.luisa.alves214@gmail.com>
2024-01-16 16:19:59 -0300

    :sparkles: feat: validação de formulario Login

--- src/pages/auth/singIn.tsx
+++ src/pages/auth/singIn.tsx
@@ -1,8 +1,28 @@
+// import { resolve } from 'path'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const singInForm = z.object({
+  email: z.string().email(),
+})
+type SingInForm = z.infer<typeof singInForm> // converte o tipo para o tipo inferido
+
 export function SingIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SingInForm>()
+
+  async function handleSingIn(data: SingInForm) {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+  }
+
   return (
     <div className="p-8 ">
       <div className="flex w-[350px] flex-col justify-center gap-6">
@@ -15,12 +35,17 @@ export function SingIn() {
           </p>
         </div>
 
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit(handleSingIn)}>
           <div className="space-y-2 ">
             <Label htmlFor="email">E-mail</Label>
-            <Input placeholder="Seu E-mail aqui" id="email" type="email" />
+            <Input
+              placeholder="Seu E-mail aqui"
+              id="email"
+              type="email"
+              {...register('email')}
+            />
           </div>
-          <Button type="submit" className="w-full">
+          <Button disabled={isSubmitting} type="submit" className="w-full">
             Acessar Painel{' '}
           </Button>
         </form>
